6fb5bd1bcef7b1e8535e63ccd89d9d7e94e2b71a
Jun <410958184@qq.com>
2017-07-07 23:58:04 +0800

    7月7日晚12点提交，ic_back 图片还没有

--- app/src/main/java/com/example/coolweather/MainActivity.java
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.coolweather;
 
+import android.support.v7.app.AlertController;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,3 +13,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+
+
